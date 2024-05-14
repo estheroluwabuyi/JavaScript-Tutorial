46e4f563958e0e38d188793f2b6e3c7ee3f4a27e
Esther Okiki <estherokiki15@gmail.com>
2024-05-14 20:41:47 +0100

    Code Challenge

--- Object-Oriented Programming/script.js
+++ Object-Oriented Programming/script.js
@@ -326,7 +326,7 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
-*/
+
 
 //INHERITANCE BTW "CLASSES": CONSTRUCTOR FUNCTIONS
 //classes here means var name
@@ -380,3 +380,52 @@ console.log(mike instanceof Object); //true
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+
+
+
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+//LINK PROTOTYPE
+//sets EV prototype to car prototype
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+    this.speed+=20;
+    // this.charge-=1
+    this.charge--;
+    console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`);
+}
+
+const tesla = new EV('Tesla', 120, 23);
+
+console.log(tesla);
+
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate(); 
+//when there are 2 methods or props with the same same name in the prototype chain, then the first one that appears in the chain is the one thats going  to be used
