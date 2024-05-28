093f0301e9ec820bb0105cd198954e6fd4963e87
Esther Okiki <estherokiki15@gmail.com>
2024-05-28 20:26:36 +0100

    ASYNCHRONOUS JS, AJAX, AND APIs

---
+++ Asynchronous/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+//
+
+const div = document.querySelector('.ii');
+// div.textContent = 'My name is Esther'
+// alert('Hey Esther👋🤪');
+// div.style.color = 'red';
+setTimeout(() => {
+    div.textContent = 'My name is Esther'
+}, 5000);
+
+div.style.color = 'red';
