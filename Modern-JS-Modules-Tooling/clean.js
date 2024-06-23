0abb8a6c0add2cfa50312ba268a1638c350d8ed6
Esther Okiki <estherokiki15@gmail.com>
2024-06-23 21:01:26 +0100

    Updated clean.js

--- Modern-JS-Modules-Tooling/clean.js
+++ Modern-JS-Modules-Tooling/clean.js
@@ -1,4 +1,7 @@
-const budget = [
+// 'strict mode'
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,44 +10,120 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
+budget[0].value = 1000;
 
 //Spending Limits
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+//By doing this, spending limit is now immutable(it has being freezed). Meaning that we can no longer put any new props into it
+
+// spendingLimits.jay = 200;
+
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+
+console.log(spendingLimits);
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+//Pure Function...no side effect
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+  // ...state creates a copy of the state array which in this case is also the budget array
 };
 
-const getLimit = user => (limit = spendingLimits?.[user] ?? 0);
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+// const checkExpenses1 = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+//   // for (const entry of budget)
+//   //   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+// };
 
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+//Impure Function...because of the console.log. All console.log create output(side effect) in the console
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`,'')
+  console.log(bigExpenses);
+
+  // let output = '';
+  // for (const entry of newBudget3)
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : ''; // Emojis count as 2 chars
+
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
 
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+logBigExpenses(finalBudget, 500);
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
 
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : ''; // Emojis count as 2 chars
 
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-console.log(budget);
-logBigExpenses(500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ryu = Object.freeze([
+//  ' sol',
+//   'jan',
+//  ' jon',
+// ])
+// console.log(ryu);
+// ryu.push('liz');
+// console.log(ryu);
