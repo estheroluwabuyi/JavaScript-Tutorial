64ed5d5d6f93c056c74220cef5f13af43f002b66
Esther Okiki <estherokiki15@gmail.com>
2024-07-04 18:43:31 +0100

    Refactoring For MVC

---
+++ Forkify/src/js/model.js
@@ -0,0 +1,33 @@
+export const state = {
+  recipe: {},
+};
+
+//function responsible for fetching the recipe data from our forkify API
+export const loadRecipe = async function (id) {
+  try{
+
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e856b'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+ const { recipe } = data.data;
+
+  state.recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+  
+  console.log(state.recipe);
+} catch(err){
+  alert(err);
+}
+};
